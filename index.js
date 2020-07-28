47f5988d12c97c0f551b71a7d76502c3fc92a855
Garrett Hudson <garretthudson87@yahoo.com>
2020-07-27 22:52:13 -0400

    mobile UI

---
+++ index.js
@@ -0,0 +1,10 @@
+var startBtn = document.getElementById("startBtn");
+var iAmIronMan = document.getElementById("ironMan");
+
+document.addEventListener("DOMContentLoaded", function(){
+  startBtn.addEventListener("click", goToGame;
+});
+
+function goToGame() {
+  iAmIronMan.onplay();
+}
